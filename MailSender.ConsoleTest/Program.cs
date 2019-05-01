99099cada7905f2bdd1e57e1f1bfe2705ebde5b4
Vasily P <flamern@hotmail.com>
2019-05-01 21:00:03 +0300

    Add folder 'test'.

---
+++ MailSender.ConsoleTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace MailSender.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (MailMessage message = new MailMessage())
+            {
+                message.From = new MailAddress("e-mail");
+                message.To.Add(new MailAddress("e-mail"));
+
+                message.Subject = "Заголовок";
+                message.Body = "Текст сообщения";
+
+                using (SmtpClient client = new SmtpClient("smtp.mail.com", 587))
+                {
+                    client.EnableSsl = true;
+                    client.Credentials = new NetworkCredential("UserName", "Password");
+                    client.Send(message);
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
